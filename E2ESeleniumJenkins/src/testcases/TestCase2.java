6abc90cdfc54b0d39738cddbea34aaa9889ac145
Peter Asp <peter.aspus@gmail.com>
2018-02-22 23:59:02 +0100

    Created new automation project

---
+++ E2ESeleniumJenkins/src/testcases/TestCase2.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+
+	@Test
+	public void testcase2()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.quit();
+	}
+
+}
---
+++ E2ESeleniumJenkins/src/testcases/TestCase4.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase4 {
+
+	@Test
+	public void testcase4()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.quit();
+	}
+
+}
