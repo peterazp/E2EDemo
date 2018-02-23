1248f75ec0e2733c3a53cadb6b186f9ed4526ac6
Peter Asp <peter.aspus@gmail.com>
2018-02-23 08:12:29 +0100

    EL-220 Correcting TC4

--- E2ESeleniumJenkins/src/testcases/TestCase4.java
+++ E2ESeleniumJenkins/src/testcases/TestCase4.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase4 {
-
+	/**
+	 * @wi.implements elibrary/EL-220 Testing RT
+	 */
 	@Test
 	public void testcase4()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.fajbook.com");
+		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("hello");
 		driver.quit();
