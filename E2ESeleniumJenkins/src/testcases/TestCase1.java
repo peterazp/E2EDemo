d93141f61ffc8894ea7f9011ea84b48b69241b06
Peter Asp <peter.aspus@gmail.com>
2018-03-06 16:13:58 +0100

    EL-243 Passing TC 1

--- E2ESeleniumJenkins/src/testcases/TestCase1.java
+++ E2ESeleniumJenkins/src/testcases/TestCase1.java
@@ -6,16 +6,18 @@ import org.testng.annotations.Test;
 public class TestCase1 {
 
 	@Test
-	
+	/**
+	 * @wi.implements elibrary/EL-243 describing method
+	 */
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.הצהצצווצהצצו.com");
+		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("helllo");
 		driver.quit();
-		// Failing TC 1
+		// Passing TC 1
 	}
 
 }
