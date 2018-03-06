c5b1b161e17dc2463769d09947d5401294494ac3
Peter Asp <peter.aspus@gmail.com>
2018-03-06 14:04:29 +0100

    EL-237 Changed login

--- E2ESeleniumJenkins/src/testcases/TestCase1.java
+++ E2ESeleniumJenkins/src/testcases/TestCase1.java
@@ -4,14 +4,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
-// New comment here
+
 	@Test
-	
+	/**
+	 * @wi.implements elibrary/EL-237 changed login
+	 */
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.הצהצווצהצהצ.com");
+		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("helllo");
 		driver.quit();
