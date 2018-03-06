a5e57390671e66f98191a8bd25019eb55f8f2dfe
Peter Asp <peter.aspus@gmail.com>
2018-03-06 12:05:34 +0100

    EL-237 Failing TC1

--- E2ESeleniumJenkins/src/testcases/TestCase1.java
+++ E2ESeleniumJenkins/src/testcases/TestCase1.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
-
+// New comment here
 	@Test
 	
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.facebook.com");
+		driver.get("http://www.הצהצווצהצהצ.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("helllo");
 		driver.quit();
