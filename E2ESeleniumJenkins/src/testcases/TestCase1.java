0534863be3dfeaff2497bf31310ea24dfeb66b90
Peter Asp <peter.aspus@gmail.com>
2018-03-08 11:35:22 +0100

    EL-254 Passing TC 1

--- E2ESeleniumJenkins/src/testcases/TestCase1.java
+++ E2ESeleniumJenkins/src/testcases/TestCase1.java
@@ -4,17 +4,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
-	
+	/**
+	 * @wi.implements elibrary/EL-254 describing method
+	 */
 	@Test
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.הצהצהצוצצהצוו.com");
+		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("helllo");
 		driver.quit();
 		
 	}
-// Failing TC 1
+// Passing TC 1
 }
