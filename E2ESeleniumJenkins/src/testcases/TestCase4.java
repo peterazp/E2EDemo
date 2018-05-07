ad3a1cf98726bf78ae4b52671fd2e5afef503360
Peter Asp <peter.aspus@gmail.com>
2018-05-07 22:07:30 +0200

    EL-268 Testing TR

--- E2ESeleniumJenkins/src/testcases/TestCase4.java
+++ E2ESeleniumJenkins/src/testcases/TestCase4.java
@@ -6,6 +6,9 @@ import org.testng.annotations.Test;
 public class TestCase4 {
 		
 	@Test
+	/**
+	* @wi.implements elibrary/EL-268 New Web Driver for Test Case 2
+	*/
 	public void testcase4()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
