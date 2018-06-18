5b012f6baa118565417b736c15ea32b45175bac8
Peter Asp <peter.aspus@gmail.com>
2018-06-18 10:56:27 +0200

    EL-47 General comment

--- E2ESeleniumJenkins/src/testcases/TestCase1.java
+++ E2ESeleniumJenkins/src/testcases/TestCase1.java
@@ -10,7 +10,7 @@ public class TestCase1 {
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.facebook.com");
+		driver.get("http://www.äöläöläöläölsdaöä.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("helllo");
 		driver.quit();
