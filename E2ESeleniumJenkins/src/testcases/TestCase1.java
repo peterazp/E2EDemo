7059fdc4d2daee0383bc09f2e49f98650e587e64
Peter Asp <peter.aspus@gmail.com>
2018-06-28 20:48:02 +0200

    comment

--- E2ESeleniumJenkins/src/testcases/TestCase1.java
+++ E2ESeleniumJenkins/src/testcases/TestCase1.java
@@ -4,20 +4,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
-	
+// hey
 	@Test
 	/**
-	* @wi.implements elibrary/EL-341 New Web Driver for Test Case 2
-	*/
+	 * @wi.implements elibrary/EL-341 general comment
+	 */
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.äöläöläöläölsdaöä.com");
+		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("hello");
 		driver.findElementById("pass").sendKeys("helllo");
 		driver.quit();
-		
+		// Hello sir
 	}
-	
+
 }
